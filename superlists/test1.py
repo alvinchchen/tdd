f18211325d003137f047daf1396396191b26324d
Alvin CH Chen (陳璟鴻) <AlvinCH_Chen@moxa.com>
2016-09-19 17:59:17 +0800

    test

--- superlists/test1.py
+++ superlists/test1.py
@@ -3,5 +3,4 @@ from selenium import webdriver
 browser = webdriver.Chrome("D:\Alvin_Chen\Downloads\chromedriver_win32\chromedriver.exe");
 browser.get('http://localhost:8000')
 assert 'Django' in browser.title
-
-
+print(1)
