fcc885e8fd4204f49a180f9be5d7e6cebf0a2db9
Alvin CH Chen (陳璟鴻) <AlvinCH_Chen@moxa.com>
2016-09-22 11:44:21 +0800

    save user data

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
-	return render(request,'home.html')
+	string = {'new_item_text' : request.POST.get('item_text',''),}
+	return render(request,'home.html', string)
--- superlists/test1.py
+++ superlists/test1.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
 		self.browser = webdriver.Chrome("D:\Alvin_Chen\Downloads\chromedriver_win32\chromedriver.exe")
@@ -9,23 +10,37 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self , row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#edith hears 
 		#
 		self.browser.get('http://localhost:8000')
 		self.assertIn('To-Do', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1').header_text
+		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do',header_text)
 
-		inputbox = self.broser.find_element_by_id('id_new_item')
+		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
 
 		inputbox.send_keys('Buy peacock feathers')
 
 		inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_element_by_tag_name('tr')
-		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+		#time.sleep(10)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peakcock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+
+
 
 		self.fail('finish the test!')
